45a5f069122803790a3ee3c721a024887d0d05bb
tubigervie <etubig@uci.edu>
2022-08-21 16:49:08 -0400

    Base enemy classes + prefabs

---
+++ Cowboy Frog/Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using UnityEngine.Rendering;
+using UnityEngine;
+
+#region REQUIRE COMPONENTS
+[RequireComponent(typeof(SortingGroup))]
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CircleCollider2D))]
+[RequireComponent(typeof(PolygonCollider2D))]
+#endregion REQUIRE COMPONENTS
+[DisallowMultipleComponent]
+public class Enemy : MonoBehaviour
+{
+    [HideInInspector] public EnemyDetailsSO enemyDetails;
+    private CircleCollider2D circleCollider2D;
+    private PolygonCollider2D polygonCollider2D;
+    [HideInInspector] public SpriteRenderer[] spriteRendererArray;
+
+    private void Awake()
+    {
+        circleCollider2D = GetComponent<CircleCollider2D>();
+        polygonCollider2D = GetComponent<PolygonCollider2D>();
+        spriteRendererArray = GetComponentsInChildren<SpriteRenderer>();
+    }
+}
---
+++ Cowboy Frog/Assets/Scripts/Enemies/EnemyDetailsSO.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="EnemyDetails_", menuName ="Scriptable Objects/Enemy/EnemyDetails")]
+public class EnemyDetailsSO : ScriptableObject
+{
+    #region Header BASE ENEMY DETAILS
+    [Space(10)]
+    [Header("BASE ENEMY DETAILS")]
+    #endregion Header BASE ENEMY DETAILS
+
+    #region Tooltip
+    [Tooltip("The name of the enemy")]
+    #endregion
+    public string enemyName;
+
+    #region Tooltip
+    [Tooltip("The prefab for the enemy")]
+    #endregion
+    public GameObject enemyPrefab;
+
+    #region Validation
+#if UNITY_EDITOR
+    private void OnValidate()
+    {
+        HelperUtilities.ValidateCheckEmptyString(this, nameof(enemyName), enemyName);
+        HelperUtilities.ValidateCheckNullValue(this, nameof(enemyPrefab), enemyPrefab);
+    }
+#endif
+    #endregion
+}
